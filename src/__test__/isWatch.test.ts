3edf618b00984e5cd442ff45e018f8ae7b56af5d
tourl-ts <18312883857@163.com>
2022-07-10 00:30:56 +0800

    feat: 检查是否获取的ref元素

--- src/__test__/isWatch.test.ts
+++ src/__test__/isWatch.test.ts
@@ -1,11 +1,13 @@
 import { describe, expect, it } from "vitest";
 import isWatch from "@/components/isWatch.vue";
 import { mount } from "@vue/test-utils";
+import { Ref } from "vue";
 
 interface wrapperType {
   count: number;
   state: { count: number };
   age: number;
+  eleRef: Ref<any>
 }
 describe("watch make firmly", () => {
   it("make watch", () => {
@@ -13,6 +15,6 @@ describe("watch make firmly", () => {
     expect((wrapper.vm as wrapperType).count).toBe(1);
     expect((wrapper.vm as wrapperType).state.count).toBe(2);
     expect((wrapper.vm as wrapperType).age).toBe(18);
-    // expect()
+    expect((wrapper.vm as wrapperType).eleRef).not.toBe(undefined);
   });
 });
