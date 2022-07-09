6ad47fc5abe4815ecae7234b9c6984da0f1dbe5a
tourl-ts <18312883857@163.com>
2022-07-09 21:39:07 +0800

    fix: 修复arguments指令选项

--- src/examples/arguments.js
+++ src/examples/arguments.js
@@ -4,7 +4,7 @@ const cli = Cli();
 cli
   // 命令的最后一个参数可以是可变参数，并且只能是最后一个参数。要使参数可变参数，您必须添加...到参数名称的开头，就像 JavaScript 中的 rest 运算符一样
   .command("build <entry> [...reset]", "Build your App")
-  .option("-foo", "Foo options")
+  .option("--foo", "Foo options")
   .action((entry, reset, options) => {
     console.log("entry", entry); // 执行命令的入口文件
     console.log("reset", reset); // 可变参数
