ea44b4e96a24320af594627d7ad6f4a284ae164d
tourl-ts <18312883857@163.com>
2022-07-12 23:58:06 +0800

    feat: 新增测试case

--- src/examples/case.js
+++ src/examples/case.js
@@ -1,11 +1,11 @@
-import Cli from 'cac';
+const Cli = require('cac');
 const cli = Cli();
 
 cli
   // 在命令名称中使用括号时，尖括号表示必需的命令参数，而方括号表示可选参数。
   .command('dev [opt]', 'Start dev server')
   // 在选项名称中使用括号时，尖括号表示需要字符串/数字值，而方括号表示该值也可以是true.
-  .option('--hello-world <foo>', 'Remove recursively')
+  .option('--hello <foo>', 'Remove recursively')
   .action(options => {
     console.log('开始actions');
     console.log(options?.helloWorld);
