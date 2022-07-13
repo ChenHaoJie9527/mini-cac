3cdfbe8639630db8679c8807899ce7d7c535731d
tourl-ts <18312883857@163.com>
2022-07-14 01:19:34 +0800

    feat: 新增生命周期测试例子

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div>
     {{ count }}
   </div>
@@ -6,4 +6,26 @@
 <script setup lang="ts">
 import { inject } from 'vue';
 const count = inject('count', 0);
+</script> -->
+
+<!-- ----------- 生命周期测试 -->
+<script setup lang="ts">
+import { onMounted, inject, onUnmounted } from 'vue';
+
+const timer = inject<any>('timer');
+const count = inject<any>('count');
+onMounted(() => {
+  timer.value = window.setInterval(() => {
+    count.value++;
+  }, 1000);
+});
+onUnmounted(() => {
+  clearInterval(timer.value);
+});
 </script>
+
+<template>
+  <div>
+    <p>Child Component: {{ count }}</p>
+  </div>
+</template>
--- src/components/isProvideInject.vue
+++ src/components/isProvideInject.vue
@@ -1,11 +1,32 @@
+<!-- <template>
+  <div>
+    <Child/>
+  </div>
+</template>
+<script setup lang="ts">
+import Child from "./Child.vue";
+import { provide, ref } from 'vue';
+const count = ref(1);
+provide('count', count);
+</script> -->
+
+<!-- -------- 声明周期例子 -->
 <template>
   <div>
-    <Child />
+    <Child v-show="visible" />
+    <button @click="onToggle">Toggle</button>
   </div>
 </template>
 <script setup lang="ts">
 import Child from './Child.vue';
 import { provide, ref } from 'vue';
-const count = ref(1);
+const visible = ref(true);
+const timer = ref(null);
+const count = ref(0);
 provide('count', count);
+provide('timer', timer);
+const onToggle = () => {
+  visible.value = !visible.value;
+}
+provide;
 </script>
