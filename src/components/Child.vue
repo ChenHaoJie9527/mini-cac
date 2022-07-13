8de7459c453a57498f48c4b7afa3b4667f1185f8
tourl-ts <18312883857@163.com>
2022-07-13 23:56:12 +0800

    feat: 依赖注入challengthes

---
+++ src/components/Child.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    {{ count }}
+  </div>
+</template>
+<script setup lang="ts">
+import { inject } from 'vue';
+const count = inject('count', 0);
+</script>
---
+++ src/components/isProvideInject.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <Child />
+  </div>
+</template>
+<script setup lang="ts">
+import Child from './Child.vue';
+import { provide, ref } from 'vue';
+const count = ref(1);
+provide('count', count);
+</script>
