05308af059f5e1d16c9c8a5cec5b25930ab0f1da
tourl-ts <18312883857@163.com>
2022-07-21 23:55:14 +0800

    feat: 新增toRaw测试

---
+++ src/__test__/isToRaw.test.ts
@@ -0,0 +1,13 @@
+import { mount } from '@vue/test-utils';
+import { describe, expect, it, vi } from 'vitest';
+import isToRaw from '@/components/ToRaw.vue';
+describe('should toRaw API', () => {
+  it('take toRaw', () => {
+    let res: string[] = [];
+    console.log = vi.fn((log: string) => {
+      res.push(log);
+    });
+    mount(isToRaw);
+    expect(JSON.stringify(res)).toBe(JSON.stringify([true, false])); // 匹配是否和isToRaw组件console的值一样
+  });
+});
