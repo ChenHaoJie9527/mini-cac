58cf475b5bff2f11a5b88ac8a0bd84d8d2323f39
tourl-ts <18312883857@163.com>
2022-07-29 00:03:01 +0800

    feat: 新增toggle

---
+++ src/components/isToggle.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>{{ state ? '1' : '2' }}</p>
+  <button @click="() => !isRef(toggle) && toggle()">toggle</button>
+</template>
+<script setup lang="ts">
+import { isRef, ref, unref } from 'vue';
+function useToggle(initialValue = false) {
+  const state = ref(initialValue);
+  function toggle() {
+    state.value = !unref(state);
+  }
+  return [state, toggle];
+}
+let [state, toggle] = useToggle(false);
+</script>
