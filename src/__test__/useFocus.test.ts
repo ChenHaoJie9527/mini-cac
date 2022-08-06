367914d17dc34e85416211945a00e475ca10062e
tourl-ts <18312883857@163.com>
2022-08-06 21:33:57 +0800

    fix: 格式代码

--- src/__test__/useFocus.test.ts
+++ src/__test__/useFocus.test.ts
@@ -3,10 +3,6 @@ import { describe, expect, it } from 'vitest';
 import useFocus from '@/components/useFocus.vue';
 describe('should useFocus', () => {
   it('test v-focus', async () => {
-    const elements = `
-    <input 
-      type="text" 
-    />`;
     const wrapper = mount(useFocus, {
       attachTo: document.body, // 指定挂载到 body 身上 可以是有效的 CSS 选择器，也可以是连接到文档的元素
     });
